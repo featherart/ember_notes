7417f5ec722d714635820e8d483d1a6bbc8b2f45
Feather Knee <fknee@netflix.com>
2014-08-05 17:59:19 -0700

    blank index

--- js/app/app.js
+++ js/app/app.js
@@ -1,3 +1,21 @@
 var Notes = Ember.Application.create({
+});
 
+// router
+Notes.Router.map(function() {
+  this.resource('notes', {path: "/"}, function() {
+    this.route('note', {path: "/note/:note_id"});
+  });
+});
+
+Notes.NotesRoute = Ember.Route.extend({
+    model: function() {
+        return this.store.find('note');
+    }
+});
+
+Notes.NotesNoteRoute = Ember.Route.extend({
+    model: function(note) {
+        return this.store.find('note', note.note_id);
+    }
 });
