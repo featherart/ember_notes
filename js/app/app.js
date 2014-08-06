10deb9d003c37af5cd0c4fd0e2eea6ff13c07181
Feather Knee <fknee@netflix.com>
2014-08-05 18:15:43 -0700

    add controller actions handler

--- js/app/app.js
+++ js/app/app.js
@@ -19,3 +19,32 @@ Notes.NotesNoteRoute = Ember.Route.extend({
         return this.store.find('note', note.note_id);
     }
 });
+
+Notes.NotesController = Ember.ArrayController.extend({
+    newNoteName: null,
+
+    actions: {
+        createNewNote: function() {
+            var content = this.get('content');
+            var newNoteName = this.get('newNoteName');
+            var unique = newNoteName != null && newNoteName.length > 1;
+
+            content.forEach(function(note) {
+                if (newNoteName === note.get('name')) {
+                    unique = false; return; // if not unique return
+                }
+            });
+
+            if (unique) {
+                var newNote = this.store.createRecord('note');
+                newNote.set('id', newNoteName);
+                newNote.set('name', newNoteName);
+                newNote.save();
+
+                this.set('newNoteName', null);
+            } else {
+                alert('Note must have a unique name of at least 2 characters!');
+            }
+        }
+    }
+});
